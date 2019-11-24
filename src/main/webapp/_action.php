74a6cf6eb7bf4d12df0aa45aaa900dc42a6fb6ca
Kamil Yeşil <kml@live.ru>
2019-11-24 19:28:33 +0300

    save ok

--- src/main/webapp/_action.php
+++ src/main/webapp/_action.php
@@ -1,11 +1,11 @@
 <?php 
 require  '../../../_SCREEN/_boot.php';
 
-$file_handle = fopen(UF.escapeshellcmd('screen.xml'), 'w');
+$file_handle = fopen(UF.escapeshellcmd('screens.xml'), 'w');
 fwrite($file_handle, urldecode($_POST["xml"]));
 fclose($file_handle);
 
-$file_handle = fopen(UF.escapeshellcmd('screen.svg'), 'w');
+$file_handle = fopen(UF.escapeshellcmd('screens.svg'), 'w');
 fwrite($file_handle, urldecode($_POST["svg"]));
 fclose($file_handle);
 
--- src/main/webapp/_pjs/main.js
+++ src/main/webapp/_pjs/main.js
@@ -1,9 +1,20 @@
+//C:\KAPPS\WWW\StaticWeb\www\pdraw\src\main\webapp\js\diagramly\Menus.js:1433
+//more shape images
+mxClient.IS_CHROMEAPP = 1;
+// Public global variables
+window.MAX_REQUEST_SIZE = window.MAX_REQUEST_SIZE  || 10485760;
+window.MAX_AREA = window.MAX_AREA || 15000 * 15000;
+
+// URLs for save and export
+window.EXPORT_URL =  '_action.php?export';
+window.SAVE_URL =  '_action.php?save';
+window.OPEN_URL =  '_action.php?open';
 
 var graphCreateSvgImageExport = Graph.prototype.createSvgImageExport;
 
 Graph.prototype.createSvgImageExport = function () {
 	var exp = graphCreateSvgImageExport.apply(this, arguments);
-	
+
 	// Overrides rendering to add metadata
 	var expDrawCellState = exp.drawCellState;
 
@@ -41,30 +52,42 @@ Graph.prototype.createSvgImageExport = function () {
 };
 
 
-//C:\KAPPS\WWW\StaticWeb\www\pdraw\src\main\webapp\js\diagramly\Menus.js:1433
-//more shape images
-mxClient.IS_CHROMEAPP=1; 
 
 var PsaveFile = App.prototype.saveFile;
-App.prototype.saveFile = function(forceDialog, success)
-{
-	var graph = this.editor.graph;
-	var svgRoot = graph.getSvg(null, null, null, null, null, true);
+App.prototype.saveFile = function (forceDialog, success) {
+	/*var graph = this.editor.graph;
+	var svgRoot = graph.getSvg(null, null, null, null, null, true)
+	mxUtils.getXml(svgRoot);*/
 
-	var r = PsaveFile.apply(this, arguments);
-	console.log(	mxUtils.getXml(svgRoot));
-	console.log(	this.getFileData(true,false));	
+	var xml = mxUtils.getXml(this.editor.getGraphXml());
+	var svg = mxUtils.getXml(this.editor.graph.getSvg(null, 1, 0))
+
 	
+	new mxXmlRequest(SAVE_URL, 'filename=' + encodeURIComponent(name) +
+						'&xml=' + encodeURIComponent(xml)+'&svg=' + encodeURIComponent(svg)).send();
+/*
+localStorage.setItem(name, xml);
+					
+
+	this.editor.setStatus(mxUtils.htmlEntities(name + " - " + mxResources.get('saved')) + ' ' + new Date());
+	var r = PsaveFile.apply(this, arguments);
+	console.log(mxUtils.getXml(svgRoot));
+	console.log(this.getFileData(false, true));
+	this.editor.setModified(false);
+	this.editor.setFilename(name);
+	this.updateDocumentTitle();
+	console.log(this);
 	return r;
+	*/
+	
 };
 
 var Pappinit = App.prototype.init;
-App.prototype.init = function()
-{
-	
+App.prototype.init = function () {
+
 
 	var r = Pappinit.apply(this, arguments);
-alert(r);
+	alert(r);
 	return r;
 };
 
