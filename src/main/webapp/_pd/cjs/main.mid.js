5509b1401e29e3f1716d9c0acc1abce9afd451fc
kml <kamilyesil@gmail.com>
2020-04-23 18:40:30 +0300

    acin fixed

--- src/main/webapp/_pd/cjs/main.mid.js
+++ src/main/webapp/_pd/cjs/main.mid.js
@@ -113,15 +113,15 @@ App.prototype.init = function()
 
 }*/
 
+//override create canvas for disable foreing object in svg. 
+var Pcreatecanvas = Graph.prototype.createSvgCanvas;
 Graph.prototype.createSvgCanvas = function (node) {
-	var canvas = new mxSvgCanvas2D(node);
+	var canvas = Pcreatecanvas.apply(this, arguments);
 	canvas.foEnabled = false;
-	canvas.pointerEvents = true;
-
-	return canvas;
+	return r;
 };
 
-
+//text input to autocomplete text input for dtag selection
 function autocomplete(inp, acarr) {
     /*the autocomplete function takes two arguments,
 	the text field element and an array of possible autocompleted values:*/
@@ -129,15 +129,15 @@ function autocomplete(inp, acarr) {
 	if(inp.tagName.toLowerCase()=='textarea')
 	inp.setAttribute('rows',1+Math.floor(inp.value.length/37));
 	var currentFocus;
-	var wrapper = document.createElement('div');
+	var wrapper = document.createElement('div'), eiwrapper, eitems;
 	// insert wrapper before el in the DOM tree
 	inp.parentNode.insertBefore(wrapper, inp);
 	// move el into wrapper
 	wrapper.appendChild(inp);
-	wrapper.className = 'autocomplete';
+	wrapper.className = 'acin';
 	/*execute a function when someone writes in the text field:*/
 	inp.addEventListener("input", function (e) {
-		var a, b, arrpos=0, vals = this.value.split(',');
+		var arrpos=0, vals = this.value.split(',');
 		if (vals.length > 1) {
 			let curpos = this.value.slice(0, this.selectionStart).length;
 			let arrcount=0;
@@ -158,11 +158,11 @@ function autocomplete(inp, acarr) {
 		 if(arr.length<1) return false;
 		currentFocus = -1;
 		/*create a DIV element that will contain the items (values):*/
-		a = document.createElement("DIV");
-		//a.setAttribute("id", this.id + "autocomplete-list");
-		a.setAttribute("class", "autocomplete-items");
+		 eiwrapper = document.createElement("DIV");
+		
+		eiwrapper.setAttribute("class", "acin-items");
 		/*append the DIV element as a child of the autocomplete container:*/
-		this.parentNode.appendChild(a);
+		this.parentNode.appendChild(eiwrapper);
 		/*for each item in the array...*/
 		for (let i = 0; i < arr.length; i++) {
 			/*check if the item starts with the same letters as the text field value:*/
@@ -170,15 +170,15 @@ function autocomplete(inp, acarr) {
 			{
 				
 				/*create a DIV element for each matching element:*/
-				b = document.createElement("DIV");
-				b.setAttribute('data',arr[i]);
+				var eitem = document.createElement("DIV");
+				eitem.setAttribute('data',arr[i]);
 				/*make the matching letters bold:*/
 				//b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-				b.innerHTML = arr[i].replace(val,'<b>' + val + '</b>');
+				eitem.innerHTML = arr[i].replace(val,'<b>' + val + '</b>');
 				/*insert a input field that will hold the current array item's value:*/
 				//b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
 				/*execute a function when someone clicks on the item value (DIV element):*/
-				b.addEventListener("click", function (e) {
+				eitem.addEventListener("click", function (e) {
 					/*insert the value for the autocomplete text field:*/
 					
 					vals[arrpos]=this.getAttribute("data");
@@ -191,64 +191,46 @@ function autocomplete(inp, acarr) {
 					if(inp.tagName.toLowerCase()=='textarea')
 					inp.setAttribute('rows',1+Math.floor(inp.value.length/37));
 				});
-				a.appendChild(b);
+				eiwrapper.appendChild(eitem);
 			}
 		}
+
+		eitems= wrapper.querySelectorAll(".acin-items div");
 	});
 	/*execute a function presses a key on the keyboard:*/
 	inp.addEventListener("keydown", function (e) {
-		var x = document.getElementById(this.id + "autocomplete-list");
-		if (x) x = x.getElementsByTagName("div");
+		if(e.keyCode>40)return;
+		if (!eitems) return false;
 		if (e.keyCode == 40) {
-			/*If the arrow DOWN key is pressed,
-			increase the currentFocus variable:*/
 			currentFocus++;
-			/*and and make the current item more visible:*/
-			addActive(x);
-		} else if (e.keyCode == 38) { //up
-			/*If the arrow UP key is pressed,
-			decrease the currentFocus variable:*/
+			addActive();
+		} else if (e.keyCode == 38) { //up	
 			currentFocus--;
-			/*and and make the current item more visible:*/
-			addActive(x);
+			addActive();
 		} else if (e.keyCode == 13) {
 			/*If the ENTER key is pressed, prevent the form from being submitted,*/
 			e.preventDefault();
-			if (currentFocus > -1) {
+			if (currentFocus > -1 ) {
 				/*and simulate a click on the "active" item:*/
-				if (x) x[currentFocus].click();
-			} else if (x.length == 1) if (x) x[0].click();
+				eitems[currentFocus].click();
+			} else if (eitems.length == 1)  eitems[0].click();
 		}
 	});
-	function addActive(x) {
-		/*a function to classify an item as "active":*/
-		if (!x) return false;
-		/*start by removing the "active" class on all items:*/
-		removeActive(x);
-		if (currentFocus >= x.length) currentFocus = 0;
-		if (currentFocus < 0) currentFocus = (x.length - 1);
+	function addActive() {
+		for (var i = 0; i < eitems.length; i++) {
+			eitems[i].classList.remove("acin-active");
+		}
+		if (currentFocus >= eitems.length) currentFocus = 0;
+		if (currentFocus < 0) currentFocus = (eitems.length - 1);
 		/*add class "autocomplete-active":*/
-		x[currentFocus].classList.add("autocomplete-active");
+		eitems[currentFocus].classList.add("acin-active");
 	}
-	function removeActive(x) {
-		/*a function to remove the "active" class from all autocomplete items:*/
-		for (var i = 0; i < x.length; i++) {
-			x[i].classList.remove("autocomplete-active");
-		}
-	}
-	function closeAllLists(elmnt) {
-		/*close all autocomplete lists in the document,
-		except the one passed as an argument:*/
-		var x = document.getElementsByClassName("autocomplete-items");
-		for (var i = 0; i < x.length; i++) {
-			if (elmnt != x[i] && elmnt != inp) {
-				x[i].parentNode.removeChild(x[i]);
-			}
-		}
+	function closeAllLists() {
+		if(eiwrapper)eiwrapper.innerHTML="";
 	}
 	/*execute a function when someone clicks in the document:*/
 	document.addEventListener("click", function (e) {
-		closeAllLists(e.target);
+		closeAllLists();
 	});
 }
 
